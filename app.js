5b07ea020d4cc0ead1e5b5170d039b8425ce5f00
Digvijay Mahadik <113425336+digvijay2100@users.noreply.github.com>
2024-07-30 09:13:05 +0530

    commit

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const port = 3000;
+
+
+
+
 require("dotenv").config();
 
 
@@ -7,23 +18,43 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client =require("twilio")(accountSid, authToken);
 
 
+app.post('/senddata', (req, res) => {
+    const { number, message } = req.body;
+    console.log(`Received number: ${number} and message: ${message}`);
+    console.log(`${number}+"****"${process.env.TO_NUMBER}`)
 
-const sendSMS = async(body)=>{
+    const sendSMS = async(body)=>{
 
-    let msgOptions ={
+        let msgOptions ={
+    
+            from:process.env.TWILIO_FROM_NUMBER,
+            to:process.env.TO_NUMBER,
+            
+            body,
+        };
+    
+        try{
+    
+            const message = await client.messages.create(msgOptions);
+            console.log(message);
+        }catch (err){
+            console.log(err);
+        }
 
-        from:process.env.TWILIO_FROM_NUMBER,
-        to:process.env.TO_NUMBER,
-        body,
+        
     };
+    
+    sendSMS(message);
+    // Respond back to the client
+    res.send(`Number received: ${number}, Message received: ${message}`);
+});
 
-    try{
 
-        const message = await client.messages.create(msgOptions);
-        console.log(message);
-    }catch (err){
-        console.log(err);
-    }
-};
 
-sendSMS("Hi, My Name is Digvijay Mahadik I have created this using twilio for sending sms using node.js");
+
+
+
+app.listen(port,()=>{
+
+    console.log('app listing at port 3000')
+})
